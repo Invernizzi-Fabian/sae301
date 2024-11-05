c969b07e60ced4685bf5dd5a2fd9e29a7c72ded6
Invernizzi-Fabian <fabian.invernizzi@edu.univ-fcomte.fr>
2024-11-05 21:14:13 +0100

    Ajout footer.php

---
+++ footer.php
@@ -0,0 +1,12 @@
+<footer class="footer">
+       <?php bloginfo('name'); ?> 2024 : Gestion des Projets.
+       <nav><?php
+          ( array (
+           'theme_location' => 'footer-menu'
+         ) ); ?></nav>
+     </footer>
+   </div>
+   <?php wp_footer(); ?>
+ </body>
+</html>
+
